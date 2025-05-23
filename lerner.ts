6cf53484e905377a0cb45583ab93e1aa085a9032
Suryakant <surajdwivedi644@gmail.com>
2025-05-23 23:49:22 +0530

    data types

--- lerner.ts
+++ lerner.ts
@@ -5,10 +5,22 @@
 // tsc -init to create a tsconfig.json file which a set of rules 
 // tsc --watch just like nodemon it will recomplie file when you make any changes in existing file
 
-a = 10
-b = 20
+// a = 10
+// b = 20
 
-a = a ^ b
-b = a ^ b
-a = a ^ b
-console.log(a, b) //
+// a = a ^ b
+// b = a ^ b
+// a = a ^ b
+// console.log(a, b) // 20 10
+
+// types of data types in typescript
+// 1. number
+// 2. string
+// 3. boolean
+// 4. null
+// 5. undefined
+
+// they are didived into two categories
+// primitive data types and reference data types
+
+// if we use [] {} () then we will say that they are references data types
